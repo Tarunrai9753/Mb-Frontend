54b6523f7db62e2d71c3e2e2a288a0c0a3ca1946
Tarunrai9753 <132670091+Tarunrai9753@users.noreply.github.com>
2024-05-30 19:58:00 +0530

    Add files via upload

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -12,7 +12,7 @@ function Login() {
   const handleSubmit = (e) => {
     e.preventDefault();
     axios
-      .post("http://localhost:3000/", { email, password })
+      .post("https://mb-backend-37on.onrender.com/", { email, password })
       .then((result) => {
         console.log(result);
         if (result.data === "success") {
